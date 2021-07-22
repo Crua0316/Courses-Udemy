554d4f575ea29c5e8185efb3c7e0e99f045f67a9
root <root@DESKTOP-7LK63D1.localdomain>
2021-07-22 11:10:07 -0500

    This function we can see how to use flow control like if, elif, else

---
+++ Flow_Control.py
@@ -0,0 +1,8 @@
+print ('Quieres comprar este videojuego\n')
+
+inputUser = input()
+if inputUser == 'Yes':
+    print ('Tú compra ha sido realizada!')
+
+elif inputUser == 'No':
+    print ('Tú compra ha sido cancelada, gracias')
