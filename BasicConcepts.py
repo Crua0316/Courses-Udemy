b754cc7334021358dc845f2c9bc5ac1c324167aa
root <root@DESKTOP-7LK63D1.localdomain>
2021-07-22 11:03:26 -0500

    This file content the concepts basic of Python

---
+++ BasicConcepts.py
@@ -0,0 +1,13 @@
+# Comments
+print (5+4)
+# Operadores
+print (5+4)
+print (5-4)
+print ((5*4)+5)
+print (5/4)
+# Operador esponencial
+print (2**5)
+# Operador cociente
+print (20//5)
+# Operador del resto
+print (20 % 6)
