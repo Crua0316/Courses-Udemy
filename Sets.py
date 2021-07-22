e316816e31e7abf9223d412771bddcce3dcb41c9
root <root@DESKTOP-7LK63D1.localdomain>
2021-07-22 11:16:28 -0500

    This function show how work the sets

---
+++ Sets.py
@@ -0,0 +1,12 @@
+Avenger =  {"Thor", "Iron man", "Capitan America"}
+print (Avenger)
+
+for count in Avenger:
+    print (count)
+
+Avenger.add("Black Widows")
+print (Avenger)
+print ("Iron man" in Avenger)
+
+for count in Avenger:
+    print (count)
